1f3d176c34087ac9e5d0b531cfec0000e05db34e
Nafas <nafiseh.ameri@partsoftware.com>
2023-07-22 01:47:00 +0330

    add map

---
+++ map.py
@@ -0,0 +1,137 @@
+# # import plotly
+# # import plotly.express as px
+# #
+# # fig = px.choropleth(locations=["Iran"], locationmode='country names')
+# #
+# # fig.show()
+#
+#
+# import pandas as pd
+# import matplotlib.pyplot as plt
+#
+# # df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+# #                    dtype={"fips": str})
+# df = pd.DataFrame({
+#     "fips": ["01001"],
+#     "unemp": [100]
+# })
+#
+# # Load the county boundary coordinates
+# # from urllib.request import urlopen
+# # import json
+# # with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+# #     counties = json.load(response)
+#
+#
+# import plotly.express as px
+#
+# mashhad = {'type': 'FeatureCollection',
+#            'features': [
+#                 {'type': 'Feature', 'id': '5725142', 'properties':
+#                 {'name': 'شهر مشهد', 'amenity': 100}, 'geometry':
+#                 {'type': 'Polygon', 'coordinates':
+#                     [[[[36.39, 59.68], [36.39, 59.68], [36.39, 59.68], [6.39, 59.68],
+#                        [36.39, 59.68], [36.39, 59.68], [36.39, 59.68], [36.39, 59.68],
+#                        [36.39, 59.68], [36.39, 59.67], [36.39, 59.67], [36.39, 59.67],
+#                        [36.39, 59.67], [36.40, 59.68], [36.40, 59.67], [36.40, 59.68],
+#                        [36.40, 59.68], [36.40, 59.68], [36.40, 59.68], [36.40, 59.68],
+#                        [36.40, 59.68], [36.40, 59.67], [36.40, 59.67], [36.40, 59.67],
+#                        [36.40, 59.67], [36.40, 59.67], [36.40, 59.67]]]]}}]}
+#
+#
+# fig = px.choropleth(df,
+#                     geojson=mashhad,
+#                     locations='fips',
+#                     color='unemp',
+#                     color_continuous_scale="Viridis",
+#                     range_color=(0, 12),
+#                     scope="asia",
+#                     labels={'unemp': 'unemployment rate'}
+#                     )
+# fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+#
+# # Improve the legend
+# fig.update_layout(coloraxis_colorbar=dict(
+#     thicknessmode="pixels", thickness=10,
+#     lenmode="pixels", len=150,
+#     yanchor="top", y=0.8,
+#     ticks="outside", ticksuffix=" %",
+#     dtick=5
+# ))
+#
+# fig.show()
+
+
+
+
+
+# import pandas as pd
+# import ast
+# import matplotlib.pyplot as plt
+# import folium
+#
+# # دیتاست را بارگیری کنید (من فرض می‌کنم دیتاست در یک فایل CSV به نام 'data.csv' ذخیره شده است)
+# data = pd.read_csv('results.csv')
+#
+# # تبدیل رشته‌های مختصات به لیست
+# data['coordinates'] = data['coordinates'].apply(lambda x: ast.literal_eval(x))
+#
+# # رسم نقشه با استفاده از folium
+# m = folium.Map(location=[36.3, 59.5], zoom_start=11)
+#
+# # تعیین رنگ بر اساس درصد و رسم نقاط بر روی نقشه
+# for index, row in data.iterrows():
+#     percentage = row['percentage']
+#     coordinates = row['coordinates']
+#     try:
+#         lat, lon = coordinates[0]['lat'], coordinates[0]['lon']
+#         color = 'green' if percentage >= 80 else 'red' if percentage >= 60 else 'blue'
+#         folium.CircleMarker(location=[lat, lon], radius=5, color=color, fill=True).add_to(m)
+#     except:
+#         pass
+#
+# # ذخیره نقشه به فایل HTML
+# m.save('map.html')
+#
+# # نمایش نقشه با matplotlib
+# plt.imshow(plt.imread('map.html'))
+# plt.axis('off')
+# plt.show()
+
+
+
+
+
+
+
+
+import pandas as pd
+import ast
+import matplotlib.pyplot as plt
+import folium
+
+# دیتاست را بارگیری کنید (من فرض می‌کنم دیتاست در یک فایل CSV به نام 'data.csv' ذخیره شده است)
+data = pd.read_csv('results (3).csv')
+
+# تبدیل رشته‌های مختصات به لیست
+data['coordinates'] = data['coordinates'].apply(lambda x: ast.literal_eval(x))
+
+# ایجاد نقشه با استفاده از folium
+m = folium.Map(location=[36.3, 59.5], zoom_start=11)
+
+# تعیین رنگ بر اساس درصد و ایجاد چند ضلعی‌ها بر روی نقشه
+for index, row in data.iterrows():
+    try:
+        percentage = row['percentage']
+        coordinates = [(coord['lat'], coord['lon']) for coord in row['coordinates']]
+        color = 'green' if percentage >= 80 else 'blue' if percentage >= 60 else 'yellow' if percentage >= 40 else 'red'
+        folium.Polygon(locations=coordinates, color=color, fill=True, fill_opacity=0.5).add_to(m)
+    except:
+        pass
+
+# ذخیره نقشه به فایل HTML
+m.save('map_with_polygons1.html')
+
+# Save the map as a PNG image directly
+image_path = 'map_with_polygons.png'
+m.save(outfile=image_path)
--- results.py
+++ results.py
@@ -20,6 +20,7 @@ import pandas as pd
 
 # ['busway', 'footway', 'bank', 'living_street', 'road', 'track', 'atm', 'bus_station', 'college', 'clinic', 'dentist', 'doctors', 'hospital', 'marketplace', 'pharmacy', 'school']
 from shapely import Polygon, wkt
+import requests
 
 category = [{'public transport': ['bureau_de_change', 'bus_station', 'car_sharing', 'busway', 'road', 'track',
                                   'motorway', 'path', 'motorcycle_parking', 'parking', 'car_rental', 'corridor',
@@ -54,7 +55,7 @@ for path, subdirs, files in os.walk('amenity/'):
     for i in range(len(files)):
         try:
             key = files[i].split('-')[0]
-            # print(files[i].split('-')[0])
+            relation_id = files[i].split('-')[1]
             name = files[i].split('-')[2]
 
             a = pd.read_csv('amenity/' + files[i])
@@ -86,6 +87,34 @@ for path, subdirs, files in os.walk('amenity/'):
             percentage = (sum(1 for c in count if c >= 1) / total_count) * 100
             count.append(round(percentage, 2))
 
+            relation = ''
+            try:
+                overpass_url = "https://overpass-api.de/api/interpreter"
+                query = f"""
+                [out:json];
+                relation({relation_id});
+                out geom;
+                """
+
+                response = requests.get(overpass_url, params={"data": query})
+                res = response.json()
+                relation = res["elements"][0]
+
+                geometry = relation['members'][0]['geometry']
+                count.append(geometry)
+                print(geometry)
+            except:
+
+                bounding_box = relation['bounds']
+                print(name, "Relation Bounds:", bounding_box)
+
+                polygon = [{'let': bounding_box['minlat'], 'lon': bounding_box['minlon']},
+                           {'let': bounding_box['minlat'], 'lon': bounding_box['maxlon']},
+                           {'let': bounding_box['maxlat'], 'lon': bounding_box['maxlon']},
+                           {'let': bounding_box['maxlat'], 'lon': bounding_box['minlon']},
+                           {'let': bounding_box['minlat'], 'lon': bounding_box['minlon']}]
+                count.append(polygon)
+
             results.__setitem__(key, count)
 
             # common_amenities = [{keywords[i]: a['count'].iloc[a['amenity'].to_list().index(keywords[i])]}
@@ -115,6 +144,7 @@ for path, subdirs, files in os.walk('amenity/'):
 # categories = ['zone']
 categories = [list(dic.keys())[0] for dic in category]
 categories.append('percentage')
+categories.append('coordinates')
 
 df = pd.DataFrame(results, index=categories)
 df = df.transpose()
@@ -122,7 +152,10 @@ df = df.transpose()
 total_results = len(results)
 percentages = []
 for key in categories:
-    percentages.append(round((sum(1 for res in df[key] if res >= 1) / total_results) * 100, 2))
+    try:
+        percentages.append(round((sum(1 for res in df[key] if res >= 1) / total_results) * 100, 2))
+    except:
+        percentages.append(0)
 
 percentages.pop()
 percentages.append(np.mean(df['percentage']))
@@ -130,102 +163,106 @@ results.__setitem__('percentage', percentages)
 
 df = pd.DataFrame(results, columns=results.keys(), index=categories)
 # print(df.head())
-# df = df.transpose()
+df = df.transpose()
 # df.to_html('results.html')
-# df.to_csv('results.csv')
+df.to_csv('results.csv', encoding='utf-8')
+# df.to_excel('results.xlsx')
 
 
-# from matplotlib import pyplot as plt
-# plt.hist(percentages, categories)
-# plt.show()
+from matplotlib import pyplot as plt
+plt.hist(percentages, categories)
+plt.show()
 
 
 
-import json
-import requests
-import pandas as pd
-
-relation_ids = [5710706, 5711010, 5711407, 5711567, 5712345, 5714586, 5714602, 5714616, 5722613, 5724225,
-                5724306, 5725071, 5725072, 8863117, 8863118, 9042658, 9042659, 9042653, 9042654, 9042650,
-                9042651, 9042652, 8834223, 8834224, 8834225, 8834226, 8834227, 9068587, 8810327, 8810328,
-                8810329, 8810330, 8873128, 8873129, 9042655, 9042656, 9042657, 9042648, 9042649, 8873125,
-                8873126, 8873127, 9042646, 9042660, 9042661, 8873134, 8873135, 8873136, 8873130, 8873131,
-                8873132, 8873133, 8796705, 8796706, 8796707, 8796708, 8796694, 8796698, 8796699, 8796700,
-                10235671, 8796695, 8796696, 8796701, 8796702, 10374714, 10374716, 8796697, 8796703, 8796704,
-                11009418, 8785942, 8785943, 8785944, 8785946, 8785947, 8785948, 8785945, 8785949, 8785950,
-                8785951, 8785952, 8824631, 8824632, 8824633, 8824629, 8824630, 8824634, 8824635, 8824636,
-                8824637, 8824638, 8824639, 8824640, 8824641, 8824642, 8824643, 8824644, 8824645, 8824646,
-                8824866, 8824867, 8824868, 8824869, 8824870, 9068585, 9068586, 8824871, 8824872, 8824873,
-                8824874, 8824875, 8824876, 8792420, 8792421, 8792422, 8792423, 8792424, 8792425, 8792426,
-                8788794, 8792427, 8788795, 8788796, 8788797, 8788798, 8788799, 8788800, 8788801, 8788802,
-                8788803, 8788804, 8788805, 8788806, 8788807, 8788808, 11009422, 11009423, 11009424, 11009428,
-                11009429, 11009430, 11009431, 11009432, 11009433, 11009435, 8810560, 8810561, 8810562, 8810563,
-                8810557, 8810558, 8810559, 8810554, 8810555, 8810556, 8833948, 8833949, 8833659, 8833660,
-                8833661, 8833662, 8833663, 8833664, 11248722, 8833665, 8833666, 8833667, 8833668, 8833669,
-                8833670, 10374717, 10374718, 8815020, 8815673, 8815674, 8815675, 8815676, 8815662, 8815668,
-                8815669, 8815670, 8815671, 8815672, 8815663, 8815664, 8815665, 8815666, 8815667, 8815016,
-                8815017, 8815018, 8815019, 8815020, 8815021, 8815013, 8815014, 8815015, 10776078, 8833775,
-                8833777, 8833778, 8833779, 11248723, 8833768, 8833769, 8833770, 8833771, 11248724, 8833772,
-                8833773, 8833774, 8833776, 8833839, 8833840, 8833841, 8833842, 8833843, 8833844, 8833845,
-                8833846, 8833847, 8833950, 9042642, 8833947, 9042643, 9042644, 9042645]
-
-
-features = []
-for relation_id in relation_ids:
-    overpass_url = "https://overpass-api.de/api/interpreter"
-    query = f"""
-    [out:json];
-    relation({relation_id});
-    out geom;
-    """
-
-    response = requests.get(overpass_url, params={"data": query})
-    data = response.json()
-
-    try:
-        relation = data["elements"][0]
-        name = relation['tags']['name']
-        geometry = relation['members'][0]['geometry']
-
-        percentage = df[name].iloc[-1]
-
-        coordinates = []
-        for geo in geometry:
-            coordinates.append([geo['lat'], geo['lon']])
-        # coordinates.append([geometry[0]['lat'], geometry[0]['lon']])
-        # print(coordinates)
-
-        polygon = Polygon(coordinates)
-        # Print the polygon as a WKT string
-        print(polygon.wkt)
-        boundary = wkt.loads(polygon.wkt)
-        print(polygon.wkt)
-
-        features.append({
-            "type": "Feature",
-            "id": str(relation_id),
-            "properties": {
-                "name": name,
-                "density": percentage
-            },
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [coordinates]
-            }
-        })
-
-    except:
-        print("Relation not found.")
-
-
-staticdata = {
-    "type": "FeatureCollection",
-    "features": features
-}
-print(staticdata)
-
-json_data = json.dumps(staticdata)
-print(json_data)
-
-with open("data.json", "w") as file:
-    file.write(json_data)
+# import json
+# import requests
+# import pandas as pd
+#
+# relation_ids = [5710706, 5711010, 5711407, 5711567, 5712345, 5714586, 5714602, 5714616, 5722613, 5724225,
+#                 5724306, 5725071, 5725072, 8863117, 8863118, 9042658, 9042659, 9042653, 9042654, 9042650,
+#                 9042651, 9042652, 8834223, 8834224, 8834225, 8834226, 8834227, 9068587, 8810327, 8810328,
+#                 8810329, 8810330, 8873128, 8873129, 9042655, 9042656, 9042657, 9042648, 9042649, 8873125,
+#                 8873126, 8873127, 9042646, 9042660, 9042661, 8873134, 8873135, 8873136, 8873130, 8873131,
+#                 8873132, 8873133, 8796705, 8796706, 8796707, 8796708, 8796694, 8796698, 8796699, 8796700,
+#                 10235671, 8796695, 8796696, 8796701, 8796702, 10374714, 10374716, 8796697, 8796703, 8796704,
+#                 11009418, 8785942, 8785943, 8785944, 8785946, 8785947, 8785948, 8785945, 8785949, 8785950,
+#                 8785951, 8785952, 8824631, 8824632, 8824633, 8824629, 8824630, 8824634, 8824635, 8824636,
+#                 8824637, 8824638, 8824639, 8824640, 8824641, 8824642, 8824643, 8824644, 8824645, 8824646,
+#                 8824866, 8824867, 8824868, 8824869, 8824870, 9068585, 9068586, 8824871, 8824872, 8824873,
+#                 8824874, 8824875, 8824876, 8792420, 8792421, 8792422, 8792423, 8792424, 8792425, 8792426,
+#                 8788794, 8792427, 8788795, 8788796, 8788797, 8788798, 8788799, 8788800, 8788801, 8788802,
+#                 8788803, 8788804, 8788805, 8788806, 8788807, 8788808, 11009422, 11009423, 11009424, 11009428,
+#                 11009429, 11009430, 11009431, 11009432, 11009433, 11009435, 8810560, 8810561, 8810562, 8810563,
+#                 8810557, 8810558, 8810559, 8810554, 8810555, 8810556, 8833948, 8833949, 8833659, 8833660,
+#                 8833661, 8833662, 8833663, 8833664, 11248722, 8833665, 8833666, 8833667, 8833668, 8833669,
+#                 8833670, 10374717, 10374718, 8815020, 8815673, 8815674, 8815675, 8815676, 8815662, 8815668,
+#                 8815669, 8815670, 8815671, 8815672, 8815663, 8815664, 8815665, 8815666, 8815667, 8815016,
+#                 8815017, 8815018, 8815019, 8815020, 8815021, 8815013, 8815014, 8815015, 10776078, 8833775,
+#                 8833777, 8833778, 8833779, 11248723, 8833768, 8833769, 8833770, 8833771, 11248724, 8833772,
+#                 8833773, 8833774, 8833776, 8833839, 8833840, 8833841, 8833842, 8833843, 8833844, 8833845,
+#                 8833846, 8833847, 8833950, 9042642, 8833947, 9042643, 9042644, 9042645]
+#
+#
+# features = []
+# for relation_id in relation_ids:
+#     overpass_url = "https://overpass-api.de/api/interpreter"
+#     query = f"""
+#     [out:json];
+#     relation({relation_id});
+#     out geom;
+#     """
+#
+#     response = requests.get(overpass_url, params={"data": query})
+#     data = response.json()
+#
+#     try:
+#         relation = data["elements"][0]
+#         name = relation['tags']['name']
+#         geometry = relation['members'][0]['geometry']
+#         # print(geometry)
+#         percentage = df[name].iloc[-1]
+#
+#         coordinates = []
+#         for geo in geometry:
+#             coordinates.append([geo['lat'], geo['lon']])
+#         # coordinates.append([geometry[0]['lat'], geometry[0]['lon']])
+#         # print(coordinates)
+#
+#         # polygon = Polygon(coordinates)
+#         # Print the polygon as a WKT string
+#         # print(polygon.wkt)
+#         # boundary = wkt.loads(polygon.wkt)
+#         # print(polygon.wkt)
+#
+#         features.append({
+#             "type": "Feature",
+#             "id": str(relation_id),
+#             "properties": {
+#                 "name": name,
+#                 "amenity": percentage
+#             },
+#             "geometry": {
+#                 "type": "Polygon",
+#                 "coordinates": [[coordinates]]
+#             }
+#         })
+#
+#     except:
+#         print("Relation not found.")
+#
+#
+# staticdata = {
+#     "type": "FeatureCollection",
+#     "features": features
+# }
+# print(staticdata)
+#
+# json_data = json.dumps(staticdata)
+# print(json_data)
+#
+# with open("data.json", "w") as file:
+#     file.write(json_data)
+#
+# with open("staticdata.json", "w") as file:
+#     file.write(staticdata)
